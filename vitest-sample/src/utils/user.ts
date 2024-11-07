f6636ada2c257aa746a496cd705a4b2ad3a1aa28
pisa-kun <pisa0401mst@gmail.com>
2024-11-07 13:36:37 +0900

    fix: spaces

--- vitest-sample/src/utils/user.ts
+++ vitest-sample/src/utils/user.ts
@@ -2,24 +2,20 @@
 export interface User {
     id: number
     name: string
-  }
-  
-  let users: User[] = []
-  
-  export function addUser(name: string): User {
+}  
+let users: User[] = []
+
+export function addUser(name: string): User {
     const newUser = { id: users.length + 1, name }
     users.push(newUser)
     return newUser
-  }
-  
-  export function getUser(id: number): User | undefined {
+}  
+export function getUser(id: number): User | undefined {
     return users.find(user => user.id === id)
-  }
-  
-  export function getAllUsers(): User[] {
+} 
+export function getAllUsers(): User[] {
     return users
-  }
-  
-  export function clearUsers() {
+}  
+export function clearUsers() {
     users = []
-  }
+}
